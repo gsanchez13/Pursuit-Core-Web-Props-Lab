96f374d49eb7b4e911f88a2bb4b91930fc0f0d77
gsanchez13 <gisellesanchez@pursuit.org>
2020-06-28 16:56:12 -0400

    imports bootstraps elements

--- go-fund-me/src/App.js
+++ go-fund-me/src/App.js
@@ -2,20 +2,21 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
+  let goal = 1000;
   let sampleDonations = [
     {
       name: 'John Doe',
-      amount: 500,
+      amount: 100,
       caption: `Let's all give to this great cause!`,
     },
     {
       name: 'John Doe',
-      amount: 500,
+      amount: 100,
       caption: `Let's all give to this great cause!`,
     },
     {
       name: 'John Doe',
-      amount: 500,
+      amount: 100,
       caption: `Let's all give to this great cause!`,
     },
   ];
@@ -28,7 +29,6 @@ const App = () => {
   const [newDonation, setNewDonation] = useState(blankDonation);
   const [totalDonations, setTotalDonations] = useState(0);
   const [totalPercentage, setTotalPercentage] = useState(0)
-  let goal = 5000;
   let jsxDonations = donations.map((donation) => {
     return (
       <div className='donation-card'>
@@ -58,48 +58,56 @@ const App = () => {
   };
   const submitNewDonation = (e) => {
     e.preventDefault();
-    if (newDonation.name && newDonation.amount && newDonation.caption) {
+    if (newDonation.name !== '' && newDonation.amount !== 0 && newDonation.caption !== '') {
       setDonations([newDonation, ...donations]);
       setNewDonation(blankDonation);
     }
   };
   return (
     <div className='container'>
-      <div className='header' style={{ textAlign: "center", marginTop: '-40px', height: '30vh', backgroundColor: '#e9ecef', paddingTop: '4vh' }}>
-        <p style={{ fontSize: '5vh', fontWeight: '10px' }}>Go Fund Me</p>
-        <p style={{ fontSize: '3vh', color: 'dimgray' }}>Help me go on a beach vacation somewhere</p>
-      </div>
+      <section class="jumbotron text-center">
+        <h1 class="jumbotron-heading">Go Fund Me</h1>
+        <p class="lead text-muted">Help me go on a vacation to a beach somewhere</p>
+      </section>
       <br />
-      <div className='grid-container' style={{ display: 'grid', gridTemplateColumns: '30% 50%', height: '60vh', gridGap: '10px', justifyContent: 'center' }}>
-        <div className='past-donations' style={{ backgroundColor: '#e9ecef', overflow: 'auto', paddingLeft: '9%', paddingRight: '9%' }}>
+      <div className='grid-container'
+        style={{ display: 'grid', gridTemplateColumns: '30% 50%', height: '60vh', gridGap: '10px', justifyContent: 'center' }}>
+        <div
+          className='past-donations'
+          style={{ backgroundColor: '#e9ecef', overflow: 'auto', paddingLeft: '9%', paddingRight: '9%' }}>
           <h3>Recent Donations </h3>
-          <div className='donations-holder' style={{ display: 'flex', justifyContent: 'center', flexFlow: 'column' }}>
+          <div className='donations-holder'
+            style={{ display: 'flex', justifyContent: 'center', flexFlow: 'column' }}>
             {jsxDonations}
           </div>
         </div>
         <div className='donation-status' style={{ paddingLeft: '5%' }}>
-          <h3>Raised ${totalDonations} of ${goal}</h3>
-          <progress value={totalDonations} max={goal} style={{ width: '100%', height: '2vh', backgroundColor: '#e9ecef', color: 'white' }} label={totalPercentage} />
+          <h3>Raised ${totalDonations} of <span class='text-muted'>${goal}</span></h3>
+          <div className='progress'>
+            <div
+              aria-valuenow={totalDonations}
+              aria-valuemax={goal}
+              aria-valuenow={totalPercentage}
+              style={{ width: { totalDonations }, height: '2vh', fontSize: '1.5vh' }}
+              className='progress-bar bg-success'
+              role="progressbar"
+            >{totalPercentage}%</div>
+          </div>
           <hr />
           <div className='donation-form'>
             <form onSubmit={submitNewDonation}>
               <label htmlFor='name'>Name</label>
-              <br />
-              <br />
-              <input id='name' type='outlined' style={{ width: '100%', height: '3vh' }} placeholder='John Doe' value={newDonation.name} onChange={updateDonation('name')} />
-              <br />
+              <input id='name' type='outlined' placeholder='John Doe' value={newDonation.name} onChange={updateDonation('name')} className='form-control' />
               <br />
               <label htmlFor='caption'>Caption</label>
-              <br />
-              <br />
-              <input id='caption' type='outlined' style={{ width: '100%', height: '3vh', textAlign: 'left', resize: 'vertical' }} maxLength='50' placeholder='...' value={newDonation.caption} onChange={updateDonation('caption')} />
+              <input id='caption' type='outlined' maxLength='50' placeholder='...' value={newDonation.caption} onChange={updateDonation('caption')} className='form-control' />
               <br />
               <br />
               <label>Amount to Donate: <b>${newDonation.amount}</b></label>
-              <input type='range' style={{ width: '100%', height: '3vh' }} min='0' max={goal} step='100' value={newDonation.amount} onChange={updateDonation('amount')}></input>
+              <input type='range' style={{ width: '100%', height: '3vh' }} min='0' max={goal} step='5' value={newDonation.amount} onChange={updateDonation('amount')} className='custom-range'></input>
               <br />
               <br />
-              <button type='submit' style={{ backgroundColor: 'green', float: 'right' }}>Donate</button>
+              <button type='submit' className='btn btn-success' style={{ float: 'right' }}>Donate</button>
             </form>
           </div>
         </div>
